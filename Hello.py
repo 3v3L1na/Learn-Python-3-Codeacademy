d6f454a1c224c4ab98a2ec68583d13d78192d4bc
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-08-18 20:53:19 +0200

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,15 @@
+my_age = 38
+print (my_age)
+
+half_my_age = (38%2)
+print (half_my_age)
+
+greeting = 'hola'
+print (greeting)
+
+name = 'Ena'
+print (name)
+
+greeting_with_name = 'hola, Ena'
+
+print (greeting_with_name)
