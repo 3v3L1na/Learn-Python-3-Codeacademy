af8d4846cd637d91ec8715761bf43a5da0697327
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-08-18 23:44:49 +0200

    Add files via upload

---
+++ #Input command.py
@@ -0,0 +1,11 @@
+#Input command
+
+likes_snakes = input("Do you like snakes? ")
+
+favorite_flightless_bird = input ("What is your favorite flightless bird?")
+
+favorite_fruit = input("What is your favorite fruit?")
+What is your favorite fruit? mango
+
+print("Oh cool! I like " + favorite_fruit + " too, but I think my favorite fruit is apple.")
+Oh cool! I like mango too, but I think my favorite fruit is apple.
