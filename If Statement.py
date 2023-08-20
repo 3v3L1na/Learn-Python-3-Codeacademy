09e98cab6c327de54693059ce2c700948a4d136c
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-08-20 23:44:49 +0200

    Add files via upload

---
+++ If Statement.py
@@ -0,0 +1,16 @@
+it_is_raining == True
+if it_is_raining == True :
+    print ("Bring an umbrella") 
+
+
+user_name == "angela_catlady_87
+
+if user_name == "Dave":
+    print ("Get off my computer, Dave!")
+    
+if user_name == "angela_catlady_87":
+   print ("I know it is you, Dave! Go away!")
+
+    
+if 2==4-2:
+    print ("apple")
