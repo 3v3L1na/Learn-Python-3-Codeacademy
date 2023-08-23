df6ef91c5102e09da497ee07b0e71d10aac7ee55
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-08-24 00:45:37 +0200

    Add files via upload

---
+++ Relational Operators II.py
@@ -0,0 +1,18 @@
+# > greater than 
+# >= greater than or equal to
+# < less than
+# <= less than or equal to
+
+age=11
+if age <=13:
+    print ("Sorry, parental control required")
+    
+x=20
+y=20
+
+if x==y:
+    print ("These numbers are the same")
+    
+credits=120
+if credits>=120: 
+    print ("You have enough credits to graduate")
