4f42ea5bc17c8584c232fc11b828abb6327914ee
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-08-24 01:02:17 +0200

    Add files via upload

---
+++ Boolean Operators-and.py
@@ -0,0 +1,13 @@
+# There are three boolean operators that we will cover: and, or, not. 
+
+# And combines two boolean expressions and evaluates as True if both are True. 
+
+statement_one = (2 + 2 + 2 >=6) and (-1 * -1 <0)
+
+statement_two = (4 * 2 <=8) and (7-1==6)
+
+credits=120
+gpa=3.4
+
+if credits >= 120 and gpa >=2.0:
+    print ("You meet the requirements to graduate!")
