e401a1a4e533c45341a00484b34fe035bd7eb2d4
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-08-25 01:21:15 +0200

    Add files via upload

---
+++ Boolean Operators-or.py
@@ -0,0 +1,14 @@
+# The boolean operator "or" combines two expressions into a larger expression. 
+
+# Oranges are a fruit or apples are a vegetable. 
+
+# Only one component needs to be True for an "or" statement to be True. 
+
+statement_one = (2-1 >3) or (-5 *2 ==-10)
+statement_two= (9+5 <=15) or (7!=4+3)
+
+credits= 118
+gpa=2.0
+
+if credits >=120 or gpa >=2.0:
+    print ("You have met at least one of the requirements")
