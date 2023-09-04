bd92e9064cddb4a2056de86eb8dbd861ae64bbde
Evelina Saponjic Jovanovic <119764859+3v3L1na@users.noreply.github.com>
2023-09-04 02:13:59 +0200

    Add files via upload

---
+++ Else statements.py
@@ -0,0 +1,11 @@
+# They elegantly describe what we want our code to do when conditions are not met.
+
+# They always appear in conjuction with if statements. 
+
+credits = 120
+gpa = 1.9
+
+if (credits >= 120) and (gpa >= 2.0):
+  print("You meet the requirements to graduate!")
+else: 
+    print ("You do not meet the requirements to graduate.")
